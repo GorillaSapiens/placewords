d45c971af65c398d36ee3a64522ca7c851c0f7b9
GorillaSapiens <embracetheape@gmail.com>
2021-05-05 15:52:04 -0400

    more

--- s2.c
+++ s2.c
@@ -2,6 +2,7 @@
 
 // TODO FIX: look for "TODO S2" and make corrections
 
+#include <stdio.h>
 #include <stdint.h>
 #include <math.h>
 
@@ -72,13 +73,97 @@ uint64_t ll_to_s2(int latE6, int lonE6) {
       result <<= 2;
       if (s >= .5L) {
          result |= 2LL;
+         s *= 2.0L;
       }
       if (t >= .5L) {
          result |= 1LL;
+         t *= 2.0L;
       }
    }
 
    return result;
 }
 
+// caution, returns static pointer to 2 ints, overwritten per call
+int *s2_to_ll(uint64_t s2) {
+   static int result[2] = { 0, 0 }; // lat, lon
 
+   double s = 0.0L, t = 0.0L;
+
+   for (int i = 0; i < 30; i++) {
+      s /= 2.0L;
+      t /= 2.0L;
+      if (s2 & 2LL) {
+         s += 0.5L;
+      }
+      if (s2 & 1LL) {
+         t += 0.5L;
+      }
+      s2 >>= 2;
+   }
+
+   double u = tan((s * 2.0L) - 1.0L) * M_PI / 4.0L;
+   double v = tan((t * 2.0L) - 1.0L) * M_PI / 4.0L;
+
+   double x, y, z;
+
+   switch(s2) {
+      case FACE_XPOS:
+         x = 1.0;
+         y = u;
+         z = v;
+         break;
+      case FACE_XNEG:
+         x = -1.0;
+         y = u;
+         z = v;
+         break;
+      case FACE_YPOS:
+         x = v;
+         y = 1.0;
+         z = u;
+         break;
+      case FACE_YNEG:
+         x = v;
+         y = -1.0;
+         z = u;
+         break;
+      case FACE_ZPOS:
+         x = u;
+         y = v; 
+         z = 1.0;
+         break;
+      case FACE_ZNEG:
+         x = u;
+         y = v; 
+         z = -1.0;
+         break;
+   }
+
+   double r = sqrt(x*x + y*y + z*z);
+   x /= r;
+   y /= r;
+   z /= r;
+
+   double lat, lon;
+
+   lon = atan2(y, x);
+   lat = atan(sqrt(x * x + y * y) / z);
+
+   result[0] = lat * 1000000.0;
+   result[1] = lon * 1000000.0;
+
+   return result;
+}
+
+void main(void) {
+   int latE6 = 37749000;
+   int lonE6 = -122419400;
+
+   uint64_t s2 = ll_to_s2(latE6, lonE6);
+
+   int *ret = s2_to_ll(s2);
+
+   printf("%d %d\n%d %d\n", latE6, lonE6, ret[0], ret[1]);
+
+}
