ab0fe2c0096aeae9d6b6ee870ec3fdacff86ca47
GorillaSapiens <embracetheape@gmail.com>
2021-05-08 19:08:11 -0400

    copyrights

--- s2.c
+++ s2.c
@@ -1,3 +1,19 @@
+// Copyright 2021 Adam Wozniak All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS-IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// Author: adam@wozniakconsulting.com (Adam Wozniak)
+
 // S2 from scratch, there may be bugs here where
 // things don't line up perfectly w/S2, but it's
 // close enough to use for debugging placewords
@@ -249,12 +265,12 @@ int *s2_to_ll(uint64_t s2) {
          break;
       case FACE_ZPOS:
          x = -u;
-         y = -v; 
+         y = -v;
          z = 1.0;
          break;
       case FACE_ZNEG:
          x = u;
-         y = v; 
+         y = v;
          z = -1.0;
          break;
    }
